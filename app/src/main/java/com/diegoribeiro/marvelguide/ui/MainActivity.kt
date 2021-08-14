92de4dc1b6b4a47aa8056ad2b10ee04bf243b897
Diego Ribeiro <dmribeiro87@gmail.com>
2021-08-14 13:41:32 -0300

    Refactor detailsFragment

--- app/src/main/java/com/diegoribeiro/marvelguide/ui/MainActivity.kt
+++ app/src/main/java/com/diegoribeiro/marvelguide/ui/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.diegoribeiro.marvelguide.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.diegoribeiro.marvelguide.R
 import com.diegoribeiro.marvelguide.data.repository.Repository
 import com.diegoribeiro.marvelguide.model.Character
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gettAll()
+        CoroutineScope(Dispatchers.IO).launch {
+            getAll()
+        }
     }
 
-    private fun gettAll(): List<Character>{
+    private suspend fun getAll(): List<Character>{
         val list = ArrayList<Character>()
-        CoroutineScope(Dispatchers.IO).launch {
+        withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
             val response = Repository.getAllCharacters()
-            if (response.isSuccessful){
-                response.body()?.let {result->
+            if (response.isSuccessful) {
+                response.body()?.let { result ->
                     list.addAll(result.data.result)
                 }
             }
