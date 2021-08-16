c941ef0320ea5fdf5857bb9f7d9fa4e3023205c9
Diego Ribeiro <dmribeiro87@gmail.com>
2021-08-16 14:01:13 -0300

    Details fragment webview

--- app/src/main/java/com/diegoribeiro/marvelguide/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/diegoribeiro/marvelguide/ui/details/DetailsFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.navigation.fragment.navArgs
+import coil.load
+import com.bumptech.glide.Glide
+import com.diegoribeiro.marvelguide.databinding.FragmentDetailsBinding
 import com.diegoribeiro.marvelguide.databinding.FragmentHomeBinding
 import com.diegoribeiro.marvelguide.ui.details.DetailsFragmentArgs
+import com.diegoribeiro.marvelguide.utils.Constants
+import com.diegoribeiro.marvelguide.utils.Constants.Companion.STANDARD_LARGE
 
 
 class DetailsFragment : Fragment() {
     private val args: DetailsFragmentArgs by navArgs()
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentDetailsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        binding.tvCurrentHeroName.text = args.currentCharacter.name
+        binding.tvCurrentOverview.text = args.currentCharacter.description
+        Glide.with(binding.ivDetails)
+            .load(args.currentCharacter.thumbnail.path + STANDARD_LARGE + ".${args.currentCharacter.thumbnail.extension}")
+            .into(binding.ivDetails)
+
+
+        binding.btCurrentInfo.setOnClickListener {
+            binding.currentExternalInfo.apply {
+                webViewClient = WebViewClient()
+                loadUrl(args.currentCharacter.urls[0].url)
+            }
+        }
 
 
         return binding.root
